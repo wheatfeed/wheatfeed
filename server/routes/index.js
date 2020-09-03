9d4a1a87d20042f174732d58eb00b6ada2043904
Brando Juberd <brandowalandouw@gmail.com>
2020-09-03 18:27:23 +0700

    ZomatoController done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const route = require('express').Router()
 const userRoute = require('./userRoute')
+const zomatoRoute = require('./zomatoRoute')
 
-route.use('/user', userRoute)
-// route.use('/zomato', zomatoRoute)
+// route.use('/user', userRoute)
+route.use('/zomato', zomatoRoute)
 // route.use('/news', newsRoute)
 // route.use('/spoonacular', spoonacularRoute)
 
