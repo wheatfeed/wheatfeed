ee119de0c221173adc0c549d447e8e5a24f4f579
arrizalrahmat <arrizalrahmat@gmail.com>
2020-09-03 19:44:07 +0700

    news done

---
+++ server/controllers/newsController.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+
+
+class NewsController {
+
+    static getNews(req, res, next) {
+        let api_key = process.env.NEWSAPIKEY
+        let url = `http://newsapi.org/v2/everything?qInTitle=culinary&from=2020-08-03&sortBy=publishedAt&apiKey=${api_key}`
+        axios({
+            method:'get',
+            url:url
+        })
+        .then(response => {
+            const data = response.data.articles
+            return res.status(200).json({ data })
+        })
+        .catch(err => {
+            return res.status(500).json(err)
+            next()
+        })
+    }
+}
+
+module.exports = NewsController
