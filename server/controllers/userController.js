c6ac10ad55714402fa1dcc25bf9c500a3ff2390f
ArdySitio <ardy.ai29@gmail.com>
2020-09-03 19:30:24 +0700

    Penambahan fitur register dan login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,47 @@
 const { User } = require('../models')
+const comparePassword = require('../helpers/comparePassword')
+const { generateToken } = require('../helpers/jwt')
 
 class UserController {
 
-    static login(req, res, next) {}
+    static register(req, res) {
+        const { name, email, password } = req.body
 
-    static register(req, res, next) {}
+        User.create({name, email, password})
+        .then(user => {
+            const {name, email} = user
+            return res.status(201).json({name, email})
+        })
+        .catch(err => {
+            return res.status(500).json({message: err})
+        })
+    }
+
+    static login(req, res) {
+        let option = {
+            where: {email: req.body.email}
+        }
+
+        User.findOne(option)
+        .then(user => {
+            if(user) {
+                let isValid = comparePassword(req.body.password, user.password)
+                if(isValid) {
+                    const access_token = generateToken(user)
+                    return res.status(200).json({access_token})
+                } else {
+                    return res.status(400).json({message: err})
+                }
+
+            } else {
+                return res.status(400).json({meesage: err})
+            }
+        })
+        .catch(err => {
+            return res.status(500).json({message: err})
+        })
+    }
 }
+
 
 module.exports = UserController
